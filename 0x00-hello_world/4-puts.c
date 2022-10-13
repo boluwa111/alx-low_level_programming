6455a7d143f0428359cd12f20ddb25bfd0324b50
boluwa111 <giwasamuel6@gmail.com>
2022-10-13 16:06:30 -0700

    ALX assignment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/*a C program that prints exactly 
+ * "Programming is like building a multilingual puzzle, 
+ * followed by a new line.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
