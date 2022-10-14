62bd3ae7a221a12ec9984965f21b0cd3b8052afe
boluwa111 <giwasamuel6@gmail.com>
2022-10-14 14:14:58 -0700

    ALX assignment

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main- a container for the program
+ * Return: 0 if the program runs else returns non zero
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,
+	\n");
+	return (0);
+}
