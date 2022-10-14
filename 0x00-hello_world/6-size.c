476606c9395ac12ac324da459aec2a1829164148
boluwa111 <giwasamuel6@gmail.com>
2022-10-14 15:02:59 -0700

    ALX assignment

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main- serves as a containner to the program.
+ * Return: 0 if the program runs else return non zero.
+ */
+int main(void)
+{
+	printf("Size of a char: %Id byte(s)\n", sizeof(char));
+	printf("size of an int: %Id byte(s)\n", sizeof(int);
+	printf("Size of a long int: %Id byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %Id byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %Id byte(s)\n", sizeof(char));
+	return (0);
+}
