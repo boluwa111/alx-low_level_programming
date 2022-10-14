3496c04bdff0802da779411fb29651ba2a138697
boluwa111 <giwasamuel6@gmail.com>
2022-10-14 13:26:28 -0700

    ALX assignment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- *main-it is a function that must be created for safe running of the function
- *return-ensure if the function runs 
- */
+*main-it is a function that must be created for safe running of the function
+*return-ensure if the function runs 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
