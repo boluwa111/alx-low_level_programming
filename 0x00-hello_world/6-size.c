d5d88c5434c97abf9b0ad52bc63c2196af365c49
boluwa111 <giwasamuel6@gmail.com>
2022-10-14 15:06:31 -0700

    ALX assignment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	printf("Size of a char: %Id byte(s)\n", sizeof(char));
-	printf("size of an int: %Id byte(s)\n", sizeof(int);
+	printf("size of an int: %Id byte(s)\n", sizeof(int));
 	printf("Size of a long int: %Id byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %Id byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %Id byte(s)\n", sizeof(char));
