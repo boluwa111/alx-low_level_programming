e957aa866cd848a5a31c5f6f6e541eec03907cca
boluwa111 <giwasamuel6@gmail.com>
2022-10-16 09:27:31 -0700

    ALX ASIGNMENT ON C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,24 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-**main- serves as the container of the function.
-**Return- 0 if the function runs else return non zero.
-*/
+ *main- serves as the container of the function.
+ *Return: 0 if the function runs else return non zero.
+**/
 int main(void)
 {
 	int n;
+	int k;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	k = n % 10;
+	if (k > 5)
 	{
-		printf("%d is positive\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, k);
 	}
-	else if (n < 0)
+	else if (k == 0)
 	{
-		printf("%d is negative\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, k);
 	}
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	else if (k < 6 && k != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, k);
 	return (0);
 }
