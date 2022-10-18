9156471ccc3f1e3088598837057231785f03d002
boluwa111 <giwasamuel6@gmail.com>
2022-10-18 12:41:47 -0700

    ALX assignment

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+/**
+ * main- this serves as the container for the program
+ * Return: 0 is returned if the code runs else return non zero value
+*/
+int main()
+{
+	printf("_putchar");
+	printf("\n");
+	return (0);
+}
