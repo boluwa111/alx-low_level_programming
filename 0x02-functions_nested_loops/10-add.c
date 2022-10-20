9b37ac423f80f4062f8b6037e9e67b5606d02bbf
boluwa111 <giwasamuel6@gmail.com>
2022-10-20 14:54:02 -0700

    ALX assignment

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add- adds two intergers.
+ * @n1: stores integer 1.
+ * @n2: stores integer 2.
+ * Description: give two integers, returns sum
+ * Return: sum
+*/
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
