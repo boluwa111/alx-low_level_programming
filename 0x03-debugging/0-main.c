24c89a9fbc4d2604c8728111b3c1769b24fa87ab
boluwa111 <giwasamuel6@gmail.com>
2022-10-21 18:40:43 -0700

    ALX assignment debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main- test if an integer is negative or positive.
+ * Return: 0 if it is true
+*/
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
