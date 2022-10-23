8023647b9f2e8f6ff96b42746d9106ee5fb346ca
boluwa111 <giwasamuel6@gmail.com>
2022-10-23 08:40:11 -0700

    ALX assignment

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print number 1-9 followed by a new line
+ * Return: return 0 if the code runs
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		_putchar(i)
+	}
+	_putchar('\n');
+
+	return (0);
+}
