ac5207bfecd1377b4d1eeb0c7b8510410ef1fde3
boluwa111 <giwasamuel6@gmail.com>
2022-10-23 14:13:28 -0700

    Alx assignment

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main"
+
+/**
+ * print_line - prints a straight line
+ * @n: indicates the length of straight line
+ * Return: always 0
+ */
+void print_line(int n)
+{
+	int i = 1;
+
+	while (i <= n)
+	{
+		if (n != 1)
+		{
+			_putchar('_');
+			i++;
+		}
+		else
+		{
+			_putchar('_');
+			break;
+		}
+	}
+}
+
