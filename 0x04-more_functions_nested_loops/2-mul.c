583f8fc00203af8997b4223d54b6201f3271ec32
boluwa111 <giwasamuel6@gmail.com>
2022-10-23 08:25:30 -0700

    ALX assignment

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - this is a function that multiplies two integers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: return the result
+*/
+int mul(int a, int b)
+{
+	int product = 0;
+
+	product = a * b;
+
+	return (product);
+}
