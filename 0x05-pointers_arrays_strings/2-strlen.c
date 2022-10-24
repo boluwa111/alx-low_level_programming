a7d31675db811282c61b9c0ea2647cfbb5cf1e27
boluwa111 <giwasamuel6@gmail.com>
2022-10-24 16:04:02 -0700

    ALX assignmnet pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string input
+ * Return: return 0
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length)
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string, in reverse,
+ * followed by a new line
+ * @s: the input string
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+
