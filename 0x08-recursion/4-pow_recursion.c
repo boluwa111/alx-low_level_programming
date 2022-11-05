7c45b576ccebe10dd5f2a979082253df485833ae
boluwa111 <giwasamuel6@gmail.com>
2022-11-05 05:09:31 -0700

    alx assignment recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that returns the
+ * value of x raised to the power of y
+ * @x: base
+ * @y: exponent
+ * Return: value of x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * (_pow_recursion(x, y - 1)));
+	}
+}
