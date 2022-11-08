66728ff2235bf137847d709d4029ae18cbaa5051
boluwa111 <giwasamuel6@gmail.com>
2022-11-08 13:05:21 -0800

    ALX assignment

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,19 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int product = 1;
-
-	if (argc == 3)
-		for (i = 1; i < 3; i++)
+	if (argc > 2)
 	{
-		product = product * atoi(argv[i]);
+		printf("%d\n", atol(argv[1]) * atol(argv[2]));
 	}
-	else (argc > 3)
+	else if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", product);
+	return (0);
 
 }
