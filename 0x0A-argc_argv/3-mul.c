a26c46729258004aa06857f47c6e27e1e1c80e91
boluwa111 <giwasamuel6@gmail.com>
2022-11-08 13:09:12 -0800

    ALX assignment

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,11 +8,12 @@
  * Return: 0 if success
  */
 
-int main(int argc, char **argv[])
+int main(int argc, char **argv)
 {
 	if (argc > 2)
 	{
 		printf("%d\n", atol(argv[1]) * atol(argv[2]));
+		return (0);
 	}
 	else if (argc < 3)
 	{
