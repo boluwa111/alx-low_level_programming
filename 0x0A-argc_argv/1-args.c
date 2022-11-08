8eb3a8fe26dd9719263da7786644f2b1399cbb97
boluwa111 <giwasamuel6@gmail.com>
2022-11-07 20:12:33 -0800

    ALX assignment

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - the main programme
+ * @argc: cont of argv
+ * @argv: the input element
+ * Return: always return 0
+ */
+int main(int argc __attribute__((unused)), char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
