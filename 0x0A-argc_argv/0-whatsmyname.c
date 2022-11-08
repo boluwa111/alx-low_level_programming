d909a9e4ce84e0856b5dea7392edb456e6afdbde
boluwa111 <giwasamuel6@gmail.com>
2022-11-07 19:39:29 -0800

    ALX assignment

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - this is where the program runs
+ * @argc: count of elements in argv
+ * @argv: array of input element
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ALX assignment on argv and *argc
