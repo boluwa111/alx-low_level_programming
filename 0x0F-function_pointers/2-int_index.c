492ca43b0e92476d1d17088c1dd9a81cd36ede3c
boluwa111 <giwasamuel6@gmail.com>
2022-11-16 13:39:02 -0800

    ALX assignment

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer.
+ * @size: number of elements in the array array
+ * @cmp: a pointer to the function to be used to compare values
+ * Return: 0 if there is matches else -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (1);
+		}
+	}
+	return(-1)
+}
