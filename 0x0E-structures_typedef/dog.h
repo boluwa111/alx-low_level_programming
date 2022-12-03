3044db3d3fdc51607a821b2176ec2218e194e88b
boluwa111 <giwasamuel6@gmail.com>
2022-12-03 04:37:03 -0800

    ALX assignment on struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Descritption: this creates a struct dog
+ */
 
 struct dog
 {
@@ -15,6 +18,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
 typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* MAIN_H */
