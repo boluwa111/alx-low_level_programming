a01fe4995acb1be96e2fe90825c6d858366abcc5
boluwa111 <giwasamuel6@gmail.com>
2022-12-03 04:24:30 -0800

    ALX assignment on struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,19 @@
 #include "dog.h"
 
-init_dog - a function that initialize a variable of type struct dog
-@name: name of dog
-@age: age of dog
-@owner: owner of the dog
+/**
+ * init_dog - a function that initialize a variable of type struct dog
+ * @d: pointer to a struct of type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->name=name;
-		d->age=age;
-		d->owner=owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
