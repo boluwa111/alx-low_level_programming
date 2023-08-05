32f2bd9623529f8e3c79f0aba8c28ef90fd80d65
boluwa111 <giwasamuel6@gmail.com>
2023-08-05 06:26:13 -0700

    alx

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,10 +7,32 @@
  * Return: 0 if success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	for (int i = 0; i < argc; i++)
-	{
-		printf("%s", argv[i])
-	}
+    int amount;
+    int change[] = {25, 10, 5, 2, 1};
+    int count = 0;
+
+    if (argc == 2)
+    {
+        amount = atoi(argv[1]);
+        for (int i = 0; i < 5; i++)
+        {
+
+            if (amount >= change[i])
+            {
+                count += (amount / change[i]);
+                amount = amount % change[i];
+            }
+            else if (amount == 0)
+            {
+                break;
+            }
+        }
+        printf("%d\n", count);
+    }
+    else
+    {
+        printf("Error\n");
+    }
 }
