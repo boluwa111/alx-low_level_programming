a8eee469a09d6e0cfe98b31fabc06f5133310422
boluwa111 <giwasamuel6@gmail.com>
2023-08-08 15:10:04 -0700

    alx

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,15 +20,13 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	int i;
+	int i, j;
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			int j;
-
 			for (j = 0; j < i; j++)
 			{
 				free(grid[j]);
@@ -36,8 +34,6 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-		int j;
-
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
