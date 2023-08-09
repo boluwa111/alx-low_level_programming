ca202dd39a8f4231f9477860f6bbc5b72be1e88a
boluwa111 <giwasamuel6@gmail.com>
2023-08-08 17:38:23 -0700

    alx

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - function that concat arg
+ * @ac: numbers of arguments
+ * @av: arguments
+ * Return: return the concat string else NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	int total_length = 0;
+	int i;
+
+	for (i = 0; i < ac; i++)
+		total_length += strlen(av[i]) + 1;
+
+	char *result = (char *)malloc(total_length * sizeof(char));
+
+	if (result == NULL)
+		return (NULL);
+	int current_index = 0;
+
+	for (i = 1; i < ac; i++)
+	{
+		strcpy(result + current_index, av[i]);
+		current_index += strlen(av[i]);
+		result[current_index] = '\n';
+		current_index++;
+	}
+	result[current_index] = '\0';
+	return (result);
+}
