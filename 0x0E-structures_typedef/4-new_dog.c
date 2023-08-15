f199490c64b47c88517f33dc76799bb607d8769d
boluwa111 <giwasamuel6@gmail.com>
2023-08-14 22:17:35 -0700

    alx

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,6 +25,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (new_dog->owner == NULL || new_dog->name == NULL)
 	{
+		free(new_dog->name);
+		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
