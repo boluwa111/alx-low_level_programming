6d451105f94a38d6ceb9b912458b5027e665a645
boluwa111 <giwasamuel6@gmail.com>
2023-09-01 09:30:05 -0700

    alx

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pre_main_message - function loads before the main function
+ */
+
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
